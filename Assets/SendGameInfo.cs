ccb335e852009e2d1bf17351fcd355f870126349
Jussi Tarvainen <jussi.tarvainen@remomedi.com>
2021-12-06 15:31:49 +0200

    use cPouta web server

--- Assets/SendGameInfo.cs
+++ Assets/SendGameInfo.cs
@@ -8,9 +8,12 @@ public class SendGameInfo : MonoBehaviour
 {
     private WWWForm form;
     private GameObject player;
+    private static string playerId = "";
+    private static string roomId = "";
     private int viewID;
     // Form address for saving player data.
-    private string gameDataURL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfbfbAE94xjXdLvtGggdgVrFi1lm1Yrv3ozDNd8K7zvga8o5w/formResponse";
+    // private string gameDataURL = "https://docs.google.com/forms/u/0/d/e/1FAIpQLSfbfbAE94xjXdLvtGggdgVrFi1lm1Yrv3ozDNd8K7zvga8o5w/formResponse";
+    private string gameDataURL = "http://128.214.253.86/api";
 
     void Start()
     {
@@ -25,19 +28,29 @@ public class SendGameInfo : MonoBehaviour
     {
         while (true)
         {
-            yield return new WaitForSeconds(0.5f);
-            form = new WWWForm();
-            form.AddField("entry.2033011394", ("Player ID: " + player.GetInstanceID() + ", x: " + player.transform.position.x.ToString("0.00") + ", y: " + player.transform.position.y.ToString("0.00")));
-            using (var w = UnityWebRequest.Post(gameDataURL, form))
+            // yield return new WaitForSeconds(0.5f); // ping every half second
+            yield return new WaitForSeconds(1f); // ping every second
+            
+            if (playerId != "") // don't ping until playerId has been set
             {
-                yield return w.SendWebRequest();
-                if (w.result != UnityWebRequest.Result.Success)
+                var pingUrl = gameDataURL + "/pingPlayer/" + playerId;
+                
+                // create form with current location
+                form = new WWWForm();
+                form.AddField("x", player.transform.position.x.ToString("0.00"));
+                form.AddField("y", player.transform.position.y.ToString("0.00"));
+                
+                using (var w = UnityWebRequest.Post(pingUrl, form))
                 {
-                    Debug.LogError("Sending information failed.");
-                }
-                else
-                {
-                    Debug.Log("Sending ping succeeded.");
+                    yield return w.SendWebRequest();
+                    if (w.result != UnityWebRequest.Result.Success)
+                    {
+                        Debug.LogError("*** Ping failed.");
+                    }
+                    // else
+                    // {
+                    //     Debug.Log("*** Ping succeeded.");
+                    // }
                 }
             }
         }
@@ -46,24 +59,25 @@ public class SendGameInfo : MonoBehaviour
     private IEnumerator SendStartInfo()
     {
         form = new WWWForm();
-        // Add start time to form.
-        form.AddField("entry.1338148533", System.DateTime.Now.ToString());
-
-        // Add player ID to form.
-        form.AddField("entry.924040491", ("Player ID: " + player.GetInstanceID()));
 
         // Add room name to form.
-        form.AddField("entry.953771128", PhotonNetwork.CurrentRoom.Name);
-        using (var w = UnityWebRequest.Post(gameDataURL, form))
+        form.AddField("name", PhotonNetwork.CurrentRoom.Name);
+
+        using (var w = UnityWebRequest.Post(gameDataURL + "/createRoom", form))
         {
             yield return w.SendWebRequest();
             if (w.result != UnityWebRequest.Result.Success)
             {
-                Debug.LogError("Sending information failed.");
+                Debug.LogError("*** Sending start information failed.");
             }
             else
             {
-                Debug.Log("Sending start information succeeded.");
+                JSONObject responseJson = new JSONObject(w.downloadHandler.text);
+                playerId = responseJson.GetField("playerId").str;
+                roomId = responseJson.GetField("roomId").str;
+                print("*** Successfully created room and player.");
+                print("*** playerId = " + playerId);
+                print("*** roomId = " + roomId);
             }
         }
     }
