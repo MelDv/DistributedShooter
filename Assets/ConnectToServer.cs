8d9d307e0fde0330f70180947be4e56517e9d5e4
Maaret Dufva <MelDv@users.noreply.github.com>
2021-12-20 00:23:42 +0200

    minor changes, deleting unnecessary files

--- Assets/ConnectToServer.cs
+++ Assets/ConnectToServer.cs
@@ -6,7 +6,6 @@ using UnityEngine.SceneManagement;
 
 public class ConnectToServer : MonoBehaviourPunCallbacks
 {
-    // Start is called before the first frame update
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
