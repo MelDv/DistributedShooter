a2ba6fb494c6056f304b6e22c30cdb00701638d3
Maaret Dufva <MelDv@users.noreply.github.com>
2021-12-20 15:01:46 +0200

    slight changes

--- Assets/WinToLobby.cs
+++ Assets/WinToLobby.cs
@@ -1,19 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 using UnityEngine.SceneManagement;
 
-public class WinToLobby : MonoBehaviour
+public class WinToLobby : MonoBehaviourPunCallbacks
 {
     // Start is called before the first frame update
     void Start()
     {
         Waiting();
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    public override void OnConnectedToMaster()
+    {
+        // Debug.Log("OnConnectedToMaster() was called by PUN.");
+        PhotonNetwork.JoinLobby();
     }
 
+    public override void OnJoinedLobby()
+    {
+        SceneManager.LoadScene("Lobby");
+    }
     private IEnumerator Waiting()
     {
         yield return new WaitForSeconds(15f);
-        SceneManager.LoadScene("Lobby");
     }
 }
